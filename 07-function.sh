9805d8f51fef9c8119d7cfabaf1f3f2f08c43118
Lav-Venkat <inukurthivenkatesh@gmail.com>
2022-09-20 19:11:47 +0530

    Added function in Linux

---
+++ 07-function.sh
@@ -0,0 +1,11 @@
+# If you assign a name to a set of commands is called as function
+# Declaration Way 1
+function SAMPLE (){
+  echo "Sample function"
+}
+
+
+# Declaration way 2
+SAMPLE1(){
+  echo "Sample1 function"
+}
