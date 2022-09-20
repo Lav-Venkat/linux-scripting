5c0bd315bba95f997cbc1e8783becd8cfa326f0b
Lav-Venkat <inukurthivenkatesh@gmail.com>
2022-09-20 12:49:40 +0530

    Special variables script added

---
+++ 05-special_varibles.sh
@@ -0,0 +1,10 @@
+echo $0 # Script Name
+echo $1 # First Argument
+echo $2 # Second Argument
+echo $3
+echo $4
+echo $* # All arguments
+echo $@ # All Arguments
+echo $# # No of Arguments
+echo $? # Status of previous executed command
+
