1fb8d724654a7437472fc523d1ee252dd01ecfa4
Lav-Venkat <inukurthivenkatesh@gmail.com>
2022-09-20 17:44:11 +0530

    If else condition practice script added

--- 06-is.sh
+++ 06-is.sh
@@ -2,4 +2,17 @@ a=$1
 if [ $a -gt 10 ]
 then
   echo "print $a"
+else
+  echo "Print $a is less than 10"
+fi
+
+x="abc"
+if [ $x == "abc"]; then
+  echo "yes, Both are equal"
+fi
+file=/tmp/new
+if[ -f $file ]; then
+  echo "File exists"
+else
+  echo "file does not exist"
 fi
