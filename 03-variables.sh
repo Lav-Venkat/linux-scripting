9113d15e4fcba720877b0046c95f1ac19818b591
Lav-Venkat <inukurthivenkatesh@gmail.com>
2022-09-20 12:30:23 +0530

    Added front end script and redirectors

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,13 @@ b=abc
 c=true
 
 echo "value of 1 = $a"
+echo "value of 1 = $b"
+echo "value of 1 = $c"
 
 # Command substitution
 
 DATE=$(date)
+echo "Today's date is $DATE"
+
+# Variable from CLI
+echo "Value of x = $x"
